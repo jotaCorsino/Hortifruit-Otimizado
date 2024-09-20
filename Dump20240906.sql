31b0fdd8f59a61a322164952b26758c87369ec69
Jaqueline Rodrigues Barbosa <134814112+JaqueRBarbosa@users.noreply.github.com>
2024-09-20 16:52:02 -0300

    Update Dump20240906.sql

--- Dump20240906.sql
+++ Dump20240906.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `sistec` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sistec`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: localhost    Database: sistec
@@ -24,7 +26,6 @@ DROP TABLE IF EXISTS `controle_estoque`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `controle_estoque` (
   `id_produto` int NOT NULL AUTO_INCREMENT,
-   id_cliente 
   `nome_produto` varchar(100) NOT NULL,
   `descricao` text,
   `quantidade` int NOT NULL,
@@ -42,6 +43,33 @@ LOCK TABLES `controle_estoque` WRITE;
 /*!40000 ALTER TABLE `controle_estoque` DISABLE KEYS */;
 INSERT INTO `controle_estoque` VALUES (1,'ype flores','detergente flore',20,3.00,'2024-09-11 17:50:26');
 /*!40000 ALTER TABLE `controle_estoque` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transacoes`
+--
+
+DROP TABLE IF EXISTS `transacoes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transacoes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `data` date NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `tipo` enum('Venda','Despesa') NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transacoes`
+--
+
+LOCK TABLES `transacoes` WRITE;
+/*!40000 ALTER TABLE `transacoes` DISABLE KEYS */;
+INSERT INTO `transacoes` VALUES (1,'2024-09-15','kiwi',25.00,'Venda');
+/*!40000 ALTER TABLE `transacoes` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -70,59 +98,6 @@ LOCK TABLES `usuarios` WRITE;
 INSERT INTO `usuarios` VALUES (1,'admin','senha123','admin'),(3,'lucas','admin123','admin'),(4,'joao','senha123','admin');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'sistec'
---
-
---
--- Dumping routines for database 'sistec'
-
-
--- Dumping client for database 'sistec'
-CREATE TABLE clientes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    telefone VARCHAR(20),
-    endereco TEXT );
-INSERT INTO clientes (id, nome, email, telefone, endereco)
-VALUES ('1', 'João Silva', 'joao.silva@hotmail.com', '123456789', 'Rua A, 123');
-INSERT INTO clientes (id, nome, email, telefone, endereco)
-VALUES ('2', Mário Augusto, 'marioaugust@gmail.com', '132425261', 'Rua C, 128');
-
---Dumping shopping for database 'sistec'
-CREATE TABLE compras (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    cliente_id INT,
-    data_compra DATE NOT NULL,
-    valor_total DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (cliente_id) REFERENCES clientes(id)
- INSERT INTO compras (cliente_id, data_compra, valor_total)
-VALUES 
-    (1, '2024-09-15', 150.00),
-    (1, '2024-09-16', 200.00),
-    (2, '2024-09-17', 75.00);
-    (2, '2024-09-05', 97.00);
-);
--- Dumping shopping items for database 'sistec'
-CREATE TABLE itens_compra (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    compra_id INT,
-    produto_id INT,
-    quantidade INT NOT NULL,
-    preco DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (compra_id) REFERENCES compras(id),
-    FOREIGN KEY (produto_id) REFERENCES controle_estoque(id_produto)
-  INSERT INTO itens_compra (compra_id, produto_id, quantidade, preco)
-VALUES 
-    (1, 1, 10, 5.00), -- Compra 1, Produto 1, 10 unidades, R$ 5,00 por unidade
-    (1, 2, 5, 10.00), -- Compra 1, Produto 2, 5 unidades, R$ 10,00 por unidade
-    (2, 1, 7, 5.00),  -- Compra 2, Produto 1, 7 unidades, R$ 5,00 por unidade
-    (3, 3, 3, 15.00); -- Compra 3, Produto 3, 3 unidades, R$ 15,00 por unidade
-
-);
-
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -133,4 +108,4 @@ VALUES
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-11 14:57:46
+-- Dump completed on 2024-09-20 16:50:54
